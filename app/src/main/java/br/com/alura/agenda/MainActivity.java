74b89ccbc42e4fba5e1668e7951312d69396e7aa
Gizele Santos <gizele.santos@ibm.com>
2019-07-23 16:04:04 -0300

    Implementando a Activity

--- app/src/main/java/br/com/alura/agenda/MainActivity.java
+++ app/src/main/java/br/com/alura/agenda/MainActivity.java
@@ -1,14 +1,7 @@
 package br.com.alura.agenda;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
 
-import android.os.Bundle;
+public class MainActivity extends Activity {
 
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
 }
